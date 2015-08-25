75967ec18fe1e9b3a81499e5f1c3cd831a0dd6eb
Hyago Basílio <hyagohba@gmail.com>
2015-08-25 11:04:29 -0300

    alterando tema

---
+++ view/default/menu.php
@@ -0,0 +1,32 @@
+<ul class="sidebar-menu">
+  <li class="header">MAIN NAVIGATION</li>
+  
+  <li class="treeview">
+    <a href="#">
+      <i class="fa fa-edit"></i> <span>Post</span>
+      <i class="fa fa-angle-left pull-right"></i>
+    </a>
+    <ul class="treeview-menu">
+      <li><a href="post.php"><i class="fa fa-circle-o"></i> Todos</a></li>
+      <li><a href="create-post.php"><i class="fa fa-circle-o"></i> Novo Post</a></li>
+    </ul>
+  </li>
+
+  <li class="treeview">
+    <a href="#">
+      <i class="fa fa-edit"></i> <span>Usuários</span>
+      <i class="fa fa-angle-left pull-right"></i>
+    </a>
+    <ul class="treeview-menu">
+      <li><a href="post.php"><i class="fa fa-circle-o"></i> Todos</a></li>
+      <li><a href="create-post.php"><i class="fa fa-circle-o"></i> Novo Post</a></li>
+    </ul>
+  </li>
+  
+  
+  <li><a href="documentation/index.html"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+  <li class="header">LABELS</li>
+  <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
+  <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
+  <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
+</ul>
